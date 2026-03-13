4716e9e014e9d3ed6fb1e1fca0f40aea16f8dc85
unknown <skdev6>
2026-03-13 08:35:32 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 ;(function($){
     $('.btn-theme-toggle').on('click', function(){
         $('html').toggleClass('light-mode');
+    });
+
+    $('.themeic-sub-menu-slide').each(function(){
+        addSubMenuSlide($(this));
     })
+
+    function addSubMenuSlide(menuWrap){
+        let subMenu = menuWrap.find('.sub-menu');
+        let menuItem = subMenu.parent('li');
+        
+    } 
+
+
 })(jQuery);
