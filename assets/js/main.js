909de4dbb0f147526b1f4be618cd3f67dc0db65b
unknown <skdev6>
2026-03-13 17:20:28 +0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,5 +13,18 @@
         
     } 
 
+    $('.header-navbar > li > .sub-menu').parent('li').hover(function(){
+        $(this).closest('.header-area-wrap').addClass('active-sub-menu');
+    }, function(){
+        $(this).closest('.header-area-wrap').removeClass('active-sub-menu');
+    })
+    $('.header-navbar .sub-menu').each(function(){
+        $(this).find('> li').each(function(index){
+            $(this).css('--index', index);
+        })
+    });
+    $('.themeic__header .sub-menu').parent('li').find('> a').each(function(){
+         
+    });
 
 })(jQuery);
